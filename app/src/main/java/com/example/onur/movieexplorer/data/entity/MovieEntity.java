506788cbd189f942ac56a322b35413c01642eb75
Onur Dasdemir <ahmetonurdasdemir@gmail.com>
2018-08-06 10:01:05 +0300

    check commit.

--- app/src/main/java/com/example/onur/movieexplorer/data/entity/MovieEntity.java
+++ app/src/main/java/com/example/onur/movieexplorer/data/entity/MovieEntity.java
@@ -1,4 +1,84 @@
 package com.example.onur.movieexplorer.data.entity;
 
+import java.util.List;
+
+
 public class MovieEntity {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Integer> genreIds = null;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
 }
